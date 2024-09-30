8d85b0d4a72643d9cbc9f82796da494a10e7c9b5
minhtaama <ngminhtam6697@gmail.com>
2024-09-30 23:46:04 +0700

    add budget_report_widget.dart

--- lib/src/common_widgets/progress_circle.dart
+++ lib/src/common_widgets/progress_circle.dart
@@ -1,4 +1,5 @@
 import 'dart:math';
+import 'dart:ui' as ui;
 
 import 'package:flutter/material.dart';
 import 'package:money_tracker_app/src/utils/extensions/context_extensions.dart';
@@ -7,35 +8,59 @@ class ProgressCircle extends CustomPainter {
   final BuildContext context;
   final Color completeColor;
   final double currentProgress;
+  final double strokeWidth;
+  final List<Color>? completeColors;
 
   ProgressCircle(
     this.context, {
     required this.currentProgress,
     required this.completeColor,
+    this.completeColors,
+    this.strokeWidth = 5,
   });
 
   @override
   void paint(Canvas canvas, Size size) {
+    final double progress = currentProgress.clamp(0, 1);
+    //final double progress = 0.85;
+
     //this is base circle
     Paint outerCircle = Paint()
-      ..strokeWidth = 5
+      ..strokeWidth = strokeWidth
       ..color = context.appTheme.onBackground.withOpacity(0.15)
       ..style = PaintingStyle.stroke;
 
+    Offset center = Offset(size.width / 2, size.height / 2);
+    double radius = min(size.width / 2, size.height / 2) - strokeWidth - 2;
+
     Paint completeArc = Paint()
-      ..strokeWidth = 5
+      ..strokeWidth = strokeWidth
       ..color = completeColor
       ..style = PaintingStyle.stroke
-      ..strokeCap = StrokeCap.round;
+      ..strokeCap = StrokeCap.butt;
 
-    Offset center = Offset(size.width / 2, size.height / 2);
-    double radius = min(size.width / 2, size.height / 2) - 7;
+    final rect = Rect.fromCircle(center: center, radius: radius);
+
+    if (completeColors != null && progress > 0.0) {
+      final colorStops = [
+        for (int i = 0; i < completeColors!.length; i++) 0 + (1 / (completeColors!.length - 1)) * i,
+      ];
+
+      final gradient = SweepGradient(
+        startAngle: 0,
+        endAngle: 2 * pi,
+        tileMode: TileMode.mirror,
+        colors: completeColors!,
+        stops: colorStops,
+        transform: const GradientRotation(-pi / 2),
+      );
+
+      completeArc.shader = gradient.createShader(rect);
+    }
 
     canvas.drawCircle(center, radius, outerCircle); // this draws main outer circle
 
-    double angle = 2 * pi * currentProgress;
-
-    canvas.drawArc(Rect.fromCircle(center: center, radius: radius), -pi / 2, angle, false, completeArc);
+    canvas.drawArc(rect, -pi / 2, 2 * pi * progress, false, completeArc);
   }
 
   @override
--- lib/src/features/reports/presentation/budget_report_widget.dart
+++ lib/src/features/reports/presentation/budget_report_widget.dart
@@ -3,12 +3,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:money_tracker_app/src/common_widgets/money_amount.dart';
+import 'package:money_tracker_app/src/common_widgets/progress_bar.dart';
 import 'package:money_tracker_app/src/common_widgets/progress_circle.dart';
 import 'package:money_tracker_app/src/common_widgets/svg_icon.dart';
 import 'package:money_tracker_app/src/features/budget/data/budget_repo.dart';
 import 'package:money_tracker_app/src/features/reports/presentation/reports_screen.dart';
+import 'package:money_tracker_app/src/utils/enums.dart';
 import 'package:money_tracker_app/src/utils/extensions/context_extensions.dart';
 import 'package:money_tracker_app/src/utils/extensions/date_time_extensions.dart';
+import 'package:money_tracker_app/src/utils/extensions/string_double_extension.dart';
 
 import '../../../common_widgets/card_item.dart';
 import '../../../common_widgets/custom_inkwell.dart';
@@ -34,108 +38,17 @@ class BudgetReportWidget extends ConsumerWidget {
         padding: const EdgeInsets.only(top: 8.0, left: 5.0, right: 5.0),
         child: Column(
           children: budgetList
-              .map((budget) => _DailyBudget(budget: budget, reportPeriod: reportPeriod, dateTimes: dateTimes))
+              .map((budget) => switch (budget.periodType) {
+                    BudgetPeriodType.daily =>
+                      _DailyBudget(budget: budget, reportPeriod: reportPeriod, dateTimes: dateTimes),
+                    BudgetPeriodType.weekly =>
+                      _WeeklyBudget(budget: budget, reportPeriod: reportPeriod, dateTimes: dateTimes),
+                    BudgetPeriodType.monthly ||
+                    BudgetPeriodType.yearly =>
+                      _MonthlyAndYearlyBudget(budget: budget, dateTimes: dateTimes),
+                  })
               .toList(),
         ),
-        //child: _DailyBudget(budgetDetail: list[0], reportPeriod: reportPeriod, dateTimes: dateTimes),
-      ),
-    );
-  }
-}
-
-class _BudgetHeader extends StatelessWidget {
-  const _BudgetHeader({super.key, required this.budget});
-
-  final BaseBudget budget;
-
-  Widget _icons(BuildContext context) {
-    return switch (budget) {
-      CategoryBudget() => Wrap(
-          alignment: WrapAlignment.end,
-          children: (budget as CategoryBudget)
-              .categories
-              .map((cat) => SvgIcon(
-                    cat.iconPath,
-                    color: context.appTheme.onBackground,
-                    size: 20,
-                    padding: const EdgeInsets.all(3),
-                  ))
-              .toList()
-            ..insert(
-              0,
-              SvgIcon(
-                AppIcons.categoriesBulk,
-                color: context.appTheme.onBackground.withOpacity(0.35),
-                padding: const EdgeInsets.only(top: 3, bottom: 3, right: 12),
-                size: 20,
-              ),
-            ),
-        ),
-      AccountBudget() => Wrap(
-          alignment: WrapAlignment.end,
-          children: (budget as AccountBudget)
-              .accounts
-              .map((acc) => SvgIcon(
-                    acc.iconPath,
-                    color: context.appTheme.onBackground,
-                    size: 20,
-                    padding: const EdgeInsets.all(3),
-                  ))
-              .toList()
-            ..insert(
-              0,
-              SvgIcon(
-                AppIcons.accountsBulk,
-                color: context.appTheme.onBackground,
-                padding: const EdgeInsets.only(top: 3, bottom: 3, right: 12),
-                size: 20,
-              ),
-            ),
-        )
-    };
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(bottom: 8.0),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  budget.name,
-                  style: kHeader2TextStyle.copyWith(color: context.appTheme.onBackground, fontSize: 14),
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                ),
-                Row(
-                  textBaseline: TextBaseline.alphabetic,
-                  crossAxisAlignment: CrossAxisAlignment.baseline,
-                  children: [
-                    Text(
-                      CalService.formatCurrency(context, budget.amount),
-                      style: kHeader1TextStyle.copyWith(color: context.appTheme.onBackground, fontSize: 14),
-                    ),
-                    Gap.w4,
-                    Text(
-                      context.appSettings.currency.code,
-                      style: kHeader4TextStyle.copyWith(color: context.appTheme.onBackground, fontSize: 14),
-                    ),
-                    Text(
-                      budget.periodType.asSuffix,
-                      style: kHeader4TextStyle.copyWith(color: context.appTheme.onBackground),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
-          Expanded(child: _icons(context)),
-        ],
       ),
     );
   }
@@ -156,11 +69,16 @@ class _DailyBudget extends ConsumerWidget {
   Widget item(BuildContext context, DateTime dateTime, BudgetServices budgetService) {
     final budgetDetail = budgetService.getBudgetDetail(context, budget, dateTime);
 
+    final colorList = budget is CategoryBudget
+        ? (budget as CategoryBudget).categories.map((e) => e.backgroundColor)
+        : (budget as AccountBudget).accounts.map((e) => e.backgroundColor);
+
     return CustomPaint(
       painter: ProgressCircle(
         context,
         currentProgress: budgetDetail.currentAmount / budgetDetail.budget.amount,
         completeColor: context.appTheme.negative,
+        completeColors: colorList.toList(),
       ),
       child: CardItem(
         color: Colors.transparent,
@@ -177,9 +95,10 @@ class _DailyBudget extends ConsumerWidget {
             child: Text(
               dateTime.day.toString(),
               style: kHeader2TextStyle.copyWith(
-                  color: dateTime.day == DateTime.now().day ? context.appTheme.primary : context.appTheme.onBackground,
-                  fontSize: 15,
-                  height: 0.99),
+                color: dateTime.day == DateTime.now().day ? context.appTheme.primary : context.appTheme.onBackground,
+                fontSize: 15,
+                height: 0.99,
+              ),
             ),
           ),
         ),
@@ -210,7 +129,17 @@ class _DailyBudget extends ConsumerWidget {
     } else {
       return [
         TableRow(children: [
-          Gap.noGap,
+          SizedBox(
+            height: 55,
+            width: 60,
+            child: Center(
+              child: Text(
+                dateTimes.first.monthToString(context, short: true),
+                style: kHeader1TextStyle.copyWith(color: context.appTheme.onBackground.withOpacity(0.65), fontSize: 15),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
           for (int i = 1; i <= 6; i++) item(context, dateTimes.first.copyWith(day: i), budgetServices)
         ]),
         TableRow(
@@ -241,7 +170,283 @@ class _DailyBudget extends ConsumerWidget {
       child: Column(
         children: [
           _BudgetHeader(budget: budget),
+          reportPeriod == ReportPeriod.week ? Gap.h12 : Gap.noGap,
           Table(children: _buildTableRow(context, budgetServices)),
+        ],
+      ),
+    );
+  }
+}
+
+class _WeeklyBudget extends ConsumerWidget {
+  const _WeeklyBudget({
+    super.key,
+    required this.budget,
+    required this.reportPeriod,
+    required this.dateTimes,
+  });
+
+  final BaseBudget budget;
+  final ReportPeriod reportPeriod;
+  final List<DateTime> dateTimes;
+
+  Widget item(BuildContext context, int i, DateTimeRange range, BudgetServices budgetServices) {
+    final budgetDetail = budgetServices.getBudgetDetail(context, budget, range.start);
+
+    return Column(
+      children: [
+        Row(
+          children: [
+            reportPeriod == ReportPeriod.week
+                ? Gap.noGap
+                : Text(
+                    context.loc.nthWeek((i + 1).toString()),
+                    style: kHeader4TextStyle.copyWith(color: context.appTheme.onBackground, fontSize: 14),
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+            const Spacer(),
+            Text(
+              '${range.start.toShortDate(context)} - ${range.end.toShortDate(context)}',
+              style: kHeader4TextStyle.copyWith(color: context.appTheme.onBackground.withOpacity(0.65), fontSize: 13),
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+            ),
+          ],
+        ),
+        Gap.h4,
+        ProgressBar(
+          color: context.appTheme.negative,
+          percentage: budgetDetail.currentAmount / budgetDetail.budget.amount,
+        ),
+        Gap.h12,
+      ],
+    );
+  }
+
+  List<Widget> _buildRow(BuildContext context, BudgetServices budgetServices) {
+    if (reportPeriod == ReportPeriod.week) {
+      return [
+        item(context, 0, DateTimeRange(start: dateTimes.first, end: dateTimes.last), budgetServices),
+      ];
+    } else {
+      final rows = <Widget>[];
+      final weeks = dateTimes.first.weekRangesInMonth(context);
+
+      for (int i = 0; i < weeks.length; i++) {
+        final range = weeks[i];
+        rows.add(
+          item(context, i, range, budgetServices),
+        );
+      }
+
+      return rows;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final budgetServices = ref.watch(budgetServicesProvider);
+
+    return Padding(
+      padding: const EdgeInsets.only(left: 8.0, right: 8.0, bottom: 16.0),
+      child: Column(
+        children: [
+          _BudgetHeader(budget: budget),
+          Gap.h12,
+          ..._buildRow(context, budgetServices),
+        ],
+      ),
+    );
+  }
+}
+
+class _MonthlyAndYearlyBudget extends ConsumerWidget {
+  const _MonthlyAndYearlyBudget({
+    super.key,
+    required this.budget,
+    required this.dateTimes,
+  });
+
+  final BaseBudget budget;
+  final List<DateTime> dateTimes;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final budgetServices = ref.watch(budgetServicesProvider);
+    final budgetDetail = budgetServices.getBudgetDetail(context, budget, dateTimes.first);
+
+    final colorList = budget is CategoryBudget
+        ? (budget as CategoryBudget).categories.map((e) => e.backgroundColor)
+        : (budget as AccountBudget).accounts.map((e) => e.backgroundColor);
+
+    return Padding(
+      padding: const EdgeInsets.only(left: 8.0, right: 8.0, bottom: 16.0),
+      child: Column(
+        children: [
+          _BudgetHeader(budget: budget),
+          Gap.h12,
+          Row(
+            children: [
+              SizedBox(
+                height: 150,
+                width: 150,
+                child: CustomPaint(
+                  painter: ProgressCircle(
+                    context,
+                    currentProgress: budgetDetail.currentAmount / budgetDetail.budget.amount,
+                    completeColor: context.appTheme.negative,
+                    completeColors: colorList.toList(),
+                    strokeWidth: 11,
+                  ),
+                  child: Center(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        budget.periodType == BudgetPeriodType.monthly
+                            ? Text(
+                                dateTimes.first.monthToString(context),
+                                style: kHeader3TextStyle.copyWith(
+                                    color: context.appTheme.onBackground.withOpacity(0.65), fontSize: 14),
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                              )
+                            : Gap.noGap,
+                        Text(
+                          dateTimes.first.year.toString(),
+                          style: kHeader1TextStyle.copyWith(
+                            color: context.appTheme.onBackground.withOpacity(0.65),
+                            fontSize: 30,
+                            height: 0.99,
+                          ),
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              const Spacer(),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  MoneyAmount(
+                    amount: budgetDetail.currentAmount,
+                    style: kHeader1TextStyle.copyWith(
+                      color: context.appTheme.onBackground.withOpacity(0.65),
+                      fontSize: 25,
+                    ),
+                  ),
+                  MoneyAmount(
+                    amount: budgetDetail.budget.amount,
+                    style: kHeader3TextStyle.copyWith(
+                      color: context.appTheme.onBackground.withOpacity(0.65),
+                      fontSize: 20,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _BudgetHeader extends StatelessWidget {
+  const _BudgetHeader({super.key, required this.budget});
+
+  final BaseBudget budget;
+
+  Widget _icons(BuildContext context) {
+    return switch (budget) {
+      CategoryBudget() => Wrap(
+          alignment: WrapAlignment.end,
+          children: (budget as CategoryBudget)
+              .categories
+              .map((cat) => SvgIcon(
+                    cat.iconPath,
+                    color: cat.backgroundColor,
+                    size: 20,
+                    padding: const EdgeInsets.all(3),
+                  ))
+              .toList()
+            ..insert(
+              0,
+              SvgIcon(
+                AppIcons.categoriesBulk,
+                color: context.appTheme.onBackground.withOpacity(0.35),
+                padding: const EdgeInsets.only(top: 3, bottom: 3, right: 12),
+                size: 20,
+              ),
+            ),
+        ),
+      AccountBudget() => Wrap(
+          alignment: WrapAlignment.end,
+          children: (budget as AccountBudget)
+              .accounts
+              .map((acc) => SvgIcon(
+                    acc.iconPath,
+                    color: acc.backgroundColor,
+                    size: 20,
+                    padding: const EdgeInsets.all(3),
+                  ))
+              .toList()
+            ..insert(
+              0,
+              SvgIcon(
+                AppIcons.accountsBulk,
+                color: context.appTheme.onBackground.withOpacity(0.35),
+                padding: const EdgeInsets.only(top: 3, bottom: 3, right: 12),
+                size: 20,
+              ),
+            ),
+        )
+    };
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 8.0, top: 6),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  budget.name,
+                  style: kHeader3TextStyle.copyWith(color: context.appTheme.onBackground, fontSize: 14),
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                Row(
+                  textBaseline: TextBaseline.alphabetic,
+                  crossAxisAlignment: CrossAxisAlignment.baseline,
+                  children: [
+                    Text(
+                      CalService.formatCurrency(context, budget.amount),
+                      style: kHeader1TextStyle.copyWith(color: context.appTheme.onBackground, fontSize: 14),
+                    ),
+                    Gap.w4,
+                    Text(
+                      context.appSettings.currency.symbol,
+                      style: kHeader4TextStyle.copyWith(color: context.appTheme.onBackground, fontSize: 14),
+                    ),
+                    Text(
+                      budget.periodType.asSuffix,
+                      style: kHeader4TextStyle.copyWith(color: context.appTheme.onBackground),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Expanded(child: _icons(context)),
         ],
       ),
     );
